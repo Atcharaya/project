45af10beb0576bf2e442fb47c1272e0de389cbb1
Marnus <58160137@go.buu.ac.th>
2018-05-21 15:31:35 +0700

    Merge branch 'master' of C:\Users\MarnusFrank\AndroidStudioProjects\project with conflicts.

---
+++ app/src/main/java/com/example/asus/project/pages/TrackRqFragment.java
@@ -0,0 +1,30 @@
+package com.example.asus.project.pages;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.asus.project.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TrackRqFragment extends Fragment {
+
+
+    public TrackRqFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_track_rq, container, false);
+    }
+
+}
