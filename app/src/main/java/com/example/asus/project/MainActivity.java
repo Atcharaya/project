1e43afef83d003e644ba0127d03594d769d0c66e
Marnus <58160137@go.buu.ac.th>
2018-05-25 00:29:11 +0700

    24/05/2561 Frank add fragment main activity

--- app/src/main/java/com/example/asus/project/MainActivity.java
+++ app/src/main/java/com/example/asus/project/MainActivity.java
@@ -100,9 +100,8 @@ public class MainActivity extends AppCompatActivity
             setTitle("หน้าหลัก");
             // Handle the camera action
         } else if (id == R.id.nav_track) {
-
-//            fragment = new TrackRqFragment();
-//            setTitle("ติดตามคำร้อง");
+            fragment = new TrackRqFragment();
+            setTitle("ติดตามคำร้อง");
         } else if (id == R.id.nav_report) {
             fragment = new SendReportFragment();
             setTitle("ใบคำร้อง/ให้คำปรึกษา");
